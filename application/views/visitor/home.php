86b4b13e4baf1d17b58017fed49cc4f4fa6d6e42
nerochaniago <nerochaniago19@gmail.com>
2019-11-22 23:02:19 +0700

    keprof

---
+++ application/views/visitor/home.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    <title>Home - Permata Kranggan Cibubur</title>
+    <style media="screen">
+    .carousel-inner{
+width:100%;
+max-height: 479px !important;
+}
+    </style>
+  </head>
+  <body>
+        <nav class="navbar navbar-light" style="background-color: #076B07; border-bottom-left-radius: 50px; height:55px;">
+          <h7 style="color:white; margin-left:25px;">permatakranggancibubur@gmail.com</h7>
+        </nav>
+    <nav class="navbar navbar-expand-sm sticky-top navbar-light" style="background-color: white; padding-top:20px; padding-bottom:20px;">
+      <a class="navbar-brand" href="#"><h3 style="margin-left:25px; color:green;">Permata Kranggan Cibubur</h3></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav" style="margin-left:50px;">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="padding-right:15px;">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item" style="padding-right:15px; padding-left:15px;">
+        <a class="nav-link" href="#">Berita</a>
+      </li>
+      <li class="nav-item" style="padding-right:15px; padding-left:15px;">
+        <a class="nav-link" href="#">Layanan</a>
+      </li>
+      <li class="nav-item" style="padding-right:15px; padding-left:15px;">
+        <a class="nav-link" href="#">Organisasi</a>
+      </li>
+      <li class="nav-item" style="padding-right:15px; padding-left:15px;">
+        <a class="nav-link" href="#">Tentang</a>
+      </li>
+      <li class="nav-item" style="padding-right:15px; padding-left:15px;">
+        <a class="nav-link" href="#">Feedback</a>
+      </li>
+    </ul>
+  </div>
+    </nav>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://miro.medium.com/max/2780/1*vai2mjbhKU3eRVNmas7saw.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://miro.medium.com/max/3200/1*TgKD2N_ZldnnOfaNztZiWQ.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.coinkolik.com/wp-content/uploads/2019/05/Ethereum-ETH-Fiyat%C4%B1-696x390.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<section class="berita" id="berita" style="background-color: rgba(204,209,209,0.3);">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2 class="mt-4 text-center">BERITA TERBARU</h2>
+        <hr style="width:300px; border-top:3px solid #666;">
+      </div>
+    </div>
+    <div class="row">
+          <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+          </div>
+          <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+          </div>
+          <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+          </div>
+          <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+          </div>
+          <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+          </div>
+          <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+          </div>
+    </div>
+  </div>
+
+</section>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
